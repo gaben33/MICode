2c79d49db7ef17867ff8c20fa1f4e6a7de990e77
Cory Lanza <corylanza@gmail.com>
2017-09-29 16:55:18 -0400

    Started Arithmetic Module

---
+++ Interpreter/MICode/Interpreter/ArithmeticModule/ArithmeticModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class ArithmeticModule : ModuleBase {
+
+    public static void Evaluate() {
+
+    }
+
+    private static Queue<Token> Tokenize(string input) {
+
+    }
+
+    private static Queue<string> ToPostFix(Queue<Token> tokens) {
+        Queue<Token> output = new Queue<Token>();
+        Stack<Operator> operators = new Stack<Operator>();
+
+        foreach(Token token in tokens) {
+            
+        }
+
+    }
+
+    private static void EvalutePostFix() {
+
+    }
+
+
+}
