d23e98620f5fe9083ced12feb224c3748c311306
Gabriel Nathan <gabenathan98@gmail.com>
2017-09-28 21:56:21 -0400

    Initial Commit

---
+++ Interpreter/MICode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MICode {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
